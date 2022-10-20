9044ae12383f192bf2dbb9655484393ec085ab06
senicko <sebastianflajszer@gmail.com>
2022-10-20 09:17:38 +0200

    Clamp player position to game area

--- src/player.rs
+++ src/player.rs
@@ -8,6 +8,9 @@ pub struct PlayerPlugin;
 #[derive(Component)]
 pub struct Player;
 
+#[derive(Component)]
+pub struct Speed(f32);
+
 /// This plugin handles player related stuff like movement
 /// Player logic is only active during the State `GameState::Playing`
 impl Plugin for PlayerPlugin {
@@ -28,27 +31,55 @@ fn spawn_player(mut commands: Commands, textures: Res<TextureAssets>) {
     commands
         .spawn_bundle(SpriteBundle {
             texture: textures.texture_bevy.clone(),
-            transform: Transform::from_translation(Vec3::new(0., 0., 1.)),
+            transform: Transform::from_translation(Vec3::new(1., 0., 1.))
+                .with_scale(Vec3::new(0.2, 0.2, 1.)),
             ..Default::default()
         })
-        .insert(Player);
+        .insert(Player)
+        .insert(Speed(150.));
 }
 
 fn move_player(
+    mut player_query: Query<(&mut Transform, &Handle<Image>, &Speed), With<Player>>,
     time: Res<Time>,
     actions: Res<Actions>,
-    mut player_query: Query<&mut Transform, With<Player>>,
+    images: Res<Assets<Image>>,
 ) {
     if actions.player_movement.is_none() {
         return;
     }
-    let speed = 150.;
+
+    let (mut player_transform, texture, speed) = player_query.single_mut();
+
+    // Player movement
     let movement = Vec3::new(
-        actions.player_movement.unwrap().x * speed * time.delta_seconds(),
-        actions.player_movement.unwrap().y * speed * time.delta_seconds(),
+        actions.player_movement.unwrap().x * speed.0 * time.delta_seconds(),
+        actions.player_movement.unwrap().y * speed.0 * time.delta_seconds(),
         0.,
     );
-    for mut player_transform in &mut player_query {
-        player_transform.translation += movement;
-    }
+
+    player_transform.translation += movement;
+
+    // Clamp player transform to game area size
+    let texture_size = images.get(texture).unwrap().texture_descriptor.size;
+
+    let player_size = Vec2::new(
+        texture_size.width as f32 * player_transform.scale.x.abs(),
+        texture_size.height as f32 as f32 * player_transform.scale.y.abs(),
+    );
+
+    // TODO: Put these values somewhere
+    let game_area = Vec2::new(300., 600.);
+
+    let bounding_box = Vec2::new(game_area.x - player_size.x, game_area.y - player_size.y);
+
+    player_transform.translation.x = player_transform
+        .translation
+        .x
+        .clamp(-bounding_box.x / 2.0, bounding_box.x / 2.0);
+
+    player_transform.translation.y = player_transform
+        .translation
+        .y
+        .clamp(-bounding_box.y / 2.0, bounding_box.y / 2.0);
 }
