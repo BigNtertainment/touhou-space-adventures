76ccaf7d195ba818263f2e8cdbb14f34e9456862
Pietrek14 <dawidekpe@gmail.com>
2022-10-20 10:35:46 +0200

    added generic despawn

---
+++ src/util.rs
@@ -0,0 +1,7 @@
+use bevy::prelude::*;
+
+fn despawn<T: Component>(mut commands: Commands, query: Query<Entity, With<T>>) {
+    for entity in query.iter() {
+        commands.entity(entity).despawn_recursive();
+    }
+}
